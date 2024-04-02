cf2085fa818c549088a1c99a5f7aa71dd1dfe3e2
hazardemircan <hazardemircan.dev@outlook.com>
2024-04-02 21:49:46 +0300

    Defined constants for db collections and models

---
+++ src/db/constants.js
@@ -0,0 +1,10 @@
+module.exports ={
+    COLLECTION_COLORS: "colors",
+    COLLECTION_BITS: "bits",
+    COLLECTION_MAPS: "maps",
+
+
+    MODEL_COLOR: "Color",
+    MODEL_BIT: "Bit",
+    MODEL_MAP: "Map"
+}
